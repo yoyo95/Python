c6d6fc6c488d67c0a3d41dd055d06204e2ab6200
Yoyo <joszer26@gmail.com>
2015-05-11 12:14:27 +0200

    Create Sigma.py

---
+++ Sigma.py
@@ -0,0 +1,11 @@
+n = 0
+g=int(input("Entrer sigma de combien ?"))
+a=1
+
+while a is not 0:
+      for i in range(1, 101):
+          n += i
+
+      print("The sum of the numbers 1 to 100 is:")
+      print(n)
+      a=int(input("changer 1/0")
